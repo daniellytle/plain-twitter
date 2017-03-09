b1c1cd5066640d6d86deca84301a51ee07c0f8b0
Dan Wilson <wilsoda@umich.edu>
2017-03-09 17:27:07 -0500

    modified for twitter use

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-}
+// PlainTwitter
+// Author: daniellytle
+
+console.log('inserting')
+var homeHTML = `
+<div class='navbox'>
+  <div class='center'>
+    <a href='/'>PlainTwitter</a>
+    <a style='float:right;' href='/'>Home</a>
+  </div>
+</div>`;
+$('#page-outer')[0].prepend($(homeHTML)[0]);
